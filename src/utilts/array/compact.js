aa0d5fc76a6d7f1dc3f3878dba5344d9cc4d3e69
jjjjph <18283190712@163.com>
2024-01-03 22:11:15 +0800

    feat: compact

---
+++ src/utilts/array/compact.js
@@ -0,0 +1,22 @@
+/*
+  compact:返回原数组中所有非假值元素组成的新数组。
+  @parmas {Array} 需要处理的数组
+*/
+
+function compact(array){
+  const result=[]
+  if(array==null){
+    return result
+  }
+  for(let i=0;i<array.length;i++){
+    if(array[i]){
+      result.push(array[i])
+    }
+  }
+  return result
+}
+
+//示例
+let arr=[0,1,'jj',null,'hello',9,7,'',false,undefined]
+let res=compact(arr)
+console.log(res);
