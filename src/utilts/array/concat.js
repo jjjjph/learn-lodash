1d42e0459a172dfd63b4a80802861f0468fd0d9c
jjjjph <18283190712@163.com>
2024-01-03 22:11:23 +0800

    feat: concat

---
+++ src/utilts/array/concat.js
@@ -0,0 +1,22 @@
+/*
+  concat: 将array与任何数组或值连接起来，返回一个新数组
+  @parmas {Array} 被连接的数组
+  @params {*} 连接的值
+*/
+
+function concat(array,...args){
+  const arr=args.reduce((arr,item)=>{
+    if(Array.isArray(item)){
+      return arr.concat(item)
+    }else{
+      return arr.concat([item])
+    }
+  },[])
+  const result=[...array,...arr]
+  return result
+}
+
+//示例
+let arr=[1]
+let res=concat(arr,'ss',[4,5],[[6]],[[[9]]])
+console.log(res);
