cab794113d95d7189bdf9db4c891004b08000807
jjjjph <18283190712@163.com>
2024-01-07 01:11:33 +0800

    feat: capitalize

---
+++ src/utilts/string/capitalize.js
@@ -0,0 +1,16 @@
+/*
+  capitalize: 传入的字符串首字母大写，剩下的小写
+  @parmas {string} 要大写开头的字符串
+*/
+
+function capitalize(str){
+  const firstWord=str.charAt(0).search(/[A-Z]/)
+  if(~firstWord){ //判断第一个字母是否为大写
+    return str.charAt(0).toUpperCase()+str.slice(1).toLowerCase()
+  } else{
+    return undefined
+  }
+}
+ //示例
+ let res=capitalize('Dskfj')
+ console.log(res);
