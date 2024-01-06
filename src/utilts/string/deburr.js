2495cc6c17b2c2f860a2aef7f00131a1e860c4c1
jjjjph <18283190712@163.com>
2024-01-07 01:32:13 +0800

    feat: deburr

---
+++ src/utilts/string/deburr.js
@@ -0,0 +1,24 @@
+/*
+  deburr: 转换字符串string中拉丁语-1补充字母 和拉丁语扩展字母-A 为基本的拉丁字母，并且去除组合变音标记
+  @parmas {string} 要改写的字符串
+*/
+
+function deburr(str) {
+  //拉丁语-1补充字母:[\u0080-\u00FF]
+  //拉丁语扩展字母-A:[\u0100-\u017F]
+  const reg = /[\u0080-\u00FF\u0100-\u017F]/g;
+
+  // 使用 String.prototype.normalize 去除组合变音标记
+  const cancleUmlaut = (str) =>
+    str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  let result = str.replace(reg, cancleUmlaut);
+  return result;
+}
+
+//"\u0300-\u036F" # 结合用读音符号
+//.normalize("NFD")方法用于查找作为参数传递给函数的字符串的 Unicode 规范化形式.NFD:Normalization form 规范分解
+//replace(old, new) 将字符串中的old字符串全部替换为new
+
+//示例
+let res = deburr("déjà vu");
+console.log(res);
