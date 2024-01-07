fbe6cc7d987fb4f421b59330581ff61edf3463ba
jjjjph <18283190712@163.com>
2024-01-07 22:12:40 +0800

    feat: escape

---
+++ src/utilts/string/escape.js
@@ -0,0 +1,20 @@
+/*
+  escape:转义string中的 "&", "<", ">", '"', "'", 和 "`" 字符为HTML实体字符
+  @parmas {String} 转义的字符串
+*/
+
+const htmlCode = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&#39;",
+};
+
+function escape(str) {
+  return str.replace(/[&<>"']/g, (chart) => htmlCode[chart]);
+}
+
+//示例
+let res = escape("< hhh");
+console.log(res);
