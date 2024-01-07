f1125d9b80052e3f20584981a020a46f3e891be0
jjjjph <18283190712@163.com>
2024-01-07 21:55:37 +0800

    feat: defaultsDeep

---
+++ src/utilts/object/defaultsDeep.js
@@ -0,0 +1,28 @@
+/*
+  defaultsDeep:类似defaults，除了它会递归分配默认属性
+  @parmas {Object} 目标对象
+  @params {Object} 来源对象
+*/
+
+function isObject(obj) {
+  return obj !== null && typeof obj === "object";
+}
+
+function defaultsDeep(obj, ...sources) {
+  sources.forEach((source) => {
+    if (source) {
+      for (const key in source) {
+        if (isObject(obj[key]) && isObject(source[key])) {
+          defaultsDeep(obj[key], source[key]); //递归调用，处理嵌套对象
+        } else { //当obj存在source的属性时，将其复制到obj中
+          obj[key] = source[key];
+        }
+      }
+    }
+  });
+  return obj;
+}
+
+//示例
+let res = defaultsDeep({ a: { b: 2 } }, { a: { b: 1, c: 3 } });
+console.log(res);
