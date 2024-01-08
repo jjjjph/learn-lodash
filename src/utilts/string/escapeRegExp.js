17bcb90a567cb1776e63c81f8202741e03a7d07f
jjjjph <18283190712@163.com>
2024-01-08 21:52:14 +0800

    feat: escapeRegExp

---
+++ src/utilts/string/escapeRegExp.js
@@ -0,0 +1,14 @@
+/*
+  escapeRegExp:转义 RegExp 字符串中特殊的字符 "^", "$", "", ".", "*", "+", "?", "(", ")", "[", "]", ", ", 和 "|"
+  @parmas {String} 转义的字符串
+*/
+
+
+
+function escapeRegExp(str) {
+  return str.replace(/[\\^$.*+?()[\]{}|]/g, (chart) =>`\\${chart}`);
+}
+
+//示例
+let res = escapeRegExp("[lodash](https://lodash.com/)");
+console.log(res);
