1538baaea1ad755159e4c4b300233c298472b1e0
jjjjph <18283190712@163.com>
2024-01-08 22:13:07 +0800

    feat: lowerFirst

---
+++ src/utilts/string/lowerFirst.js
@@ -0,0 +1,12 @@
+/*
+  lowerFirst: 转换字符串string的首字母为小写
+  @parmas {string} 要改写的字符串
+*/
+
+function lowerFirst(str){
+  return str[0].toLowerCase()+str.slice(1)
+}
+
+//示例
+let res=lowerFirst('FFFFBar')
+console.log(res);
