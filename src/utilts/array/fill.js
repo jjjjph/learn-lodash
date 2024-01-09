b63942a26970c851dbc4aa54676c9e4d5b127715
jjjjph <139859634+jjjjph@users.noreply.github.com>
2024-01-09 20:29:14 +0800

    Update fill.js

--- src/utilts/array/fill.js
+++ src/utilts/array/fill.js
@@ -1,9 +1,9 @@
 /*
   fill:使用 value 值来填充（替换） array，从start位置开始, 到end位置结束（但不包含end位置）
-  @parmas {Array} 要填充改变的数组
-  @parmas {*} 要填充的值
-  @parmas {Number} 开始的位置
-  @parmas {Number} 结束的位置
+  @params {Array} 要填充改变的数组
+  @params {*} 要填充的值
+  @params {Number} 开始的位置
+  @params {Number} 结束的位置
 */
 
 function fill(arr, value, start, end) {
