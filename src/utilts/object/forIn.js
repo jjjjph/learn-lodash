d4575c6b1f609225529bf81827976d195ed08d8c
jjjjph <18283190712@163.com>
2024-01-10 21:56:03 +0800

    feat: forIn

---
+++ src/utilts/object/forIn.js
@@ -0,0 +1,22 @@
+/*
+  forIn:使用 iteratee 遍历对象的自身和继承的可枚举属性。 iteratee 会传入3个参数：(value, key, object)。 如果返回 false，iteratee 会提前退出遍历
+*/
+//forInRight:这个方法类似forIn。 除了它是反方向开始遍历object的
+
+function forIn(obj,predicate){
+  for(const key in obj){
+    predicate(obj[key],key,obj)
+  }
+}
+
+
+//示例
+function Foo() {
+  this.a = 1;
+  this.b = 2;
+}
+
+Foo.prototype.c = 3;
+let res=forIn(new Foo,function(value, key) {
+  console.log(key);
+});
