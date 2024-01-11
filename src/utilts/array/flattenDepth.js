b2e89498dceb9e48905d8cb986cc16489eb0e242
jjjjph <18283190712@163.com>
2024-01-11 22:04:05 +0800

    feat: flattenDepth

---
+++ src/utilts/array/flattenDepth.js
@@ -0,0 +1,20 @@
+/*
+  flattenDepth: 根据depth来递归减少array的嵌套层级
+  @params {Array} 需要展开的数组
+  @params {Number} 最多减少的嵌套层级
+*/
+
+function flattenDepth(arr, depth) {
+  const dep = depth === undefined ? 1 : depth;
+  return arr.reduce((res, item) => {
+    if (Array.isArray(item) && depth > 1) {
+      return res.concat(flattenDepth(item, depth - 1));
+    } else {
+      return res.concat(item);
+    }
+  }, []);
+}
+
+//示例
+let res = flattenDepth([1, [2, [3, [4]], 5]], 2);
+console.log(res);
