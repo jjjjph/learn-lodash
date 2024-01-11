effdfac413e8252ebbbe1c5d836c494ad2e1aca0
jjjjph <18283190712@163.com>
2024-01-11 21:52:21 +0800

    feat: flattenDeep

---
+++ src/utilts/array/flattenDeep.js
@@ -0,0 +1,18 @@
+/*
+  flattenDeep:展开成一维数组的数组
+  @params {Array} 需要展开成一维数组的数组
+*/
+
+function flattenDeep(arr){
+  return arr.reduce((res,item)=>{
+    if(Array.isArray(item)){
+      return res.concat(flattenDeep(item))
+    }else{
+      return res.concat(item)
+    }
+  },[])
+}
+
+//示例
+let res=flattenDeep([1,2,[3],[4,[5,6,[7,[8,[9]]]]]])
+console.log(res);
