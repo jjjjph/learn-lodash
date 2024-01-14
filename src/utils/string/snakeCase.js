310bf428cd6a41381ee5dcbceb6b540345d8947b
jjjjph <18283190712@163.com>
2024-01-14 17:34:20 +0800

    fix: snakeCase

--- src/utils/string/snakeCase.js
+++ src/utils/string/snakeCase.js
@@ -3,24 +3,26 @@
   @params {String} 要转换的字符串
 */
 
-function snakeCase(str){
-  if(!str){
-    return ''
+function snakeCase(str) {
+  if (!str) {
+    return "";
   }
-  const newstr=str.replace(/[A-Z]/,(chart)=>`_${chart.toLowerCase()}`)
-  const words=newstr.split(/[-_\s]+/).filter(item=>{
-    if(item){
-      return item
+  const reg = /[-_]+/;
+  let newstr;
+  if (reg.test(str)) {
+    newstr = str.replace(/[A-Z]/, (chart) => `_${chart.toLowerCase()}`);
+  } else {
+    newstr = str.replace(/[A-Z]/g, (chart) => `_${chart.toLowerCase()}`);
+  }
+  const words = newstr.split(/[-_\s]+/).filter((item) => {
+    if (item) {
+      return item;
     }
-  })  //将字符串分割成单词数组
+  }); //将字符串分割成单词数组
 
-
-
-  return words.map(str=>str.toLowerCase()).join("_")
-
-
+  return words.map((str) => str.toLowerCase()).join("_");
 }
 
 //示例
-let res=snakeCase('__FOO_BAR__')
+let res = snakeCase("dshHggdJtgHjj");
 console.log(res);
