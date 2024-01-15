06e81d5b391cbcea1b62ca5aeba8cbbd6dbba73f
jjjjph <18283190712@163.com>
2024-01-15 22:09:09 +0800

    feat: initial

---
+++ src/utils/array/initial.js
@@ -0,0 +1,19 @@
+/*
+  initial:去除数组的最后的一个元素
+  @params {Array} 要查询的数组
+*/
+
+function initial(arr){
+  const length=arr.length
+  if(!length){
+    return
+  }
+
+  // return arr.slice(0,length-1)
+  const result=arr.splice(0,length-1)
+  return result
+}
+
+//示例
+let res=initial([1, 2, 3])
+console.log(res);
