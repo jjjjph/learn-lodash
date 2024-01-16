94ff565f311cd11687a033ade4bb1502dff258aa
jjjjph <18283190712@163.com>
2024-01-16 21:46:20 +0800

    feat: last

---
+++ src/utils/array/last.js
@@ -0,0 +1,16 @@
+/*
+  last:获取数组 array 的第一个元素
+  @params {Array} 要查询的数组
+*/
+
+function last(arr){
+  const length=arr.length
+  if(!length){
+    return undefined
+  }
+  return arr[length-1]
+}
+
+//示例
+let res=last(['w','d','s'])
+console.log(res);
